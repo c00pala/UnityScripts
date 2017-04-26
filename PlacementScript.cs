d0b8d19d97fb5c24a92fddb3e978beb25a21ba19
c00pala <c00p.b.coding@gmail.com>
2017-04-26 11:29:51 +1000

    Added File.

---
+++ PlacementScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacementScript : MonoBehaviour {
+
+    private int selectedObjectInArray;
+    private GameObject currentlySelectedObject;
+
+    [SerializeField]
+    private GameObject[] selectableObjects;
+
+    private bool isAnObjectSelected = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        selectedObjectInArray = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 spawnPos = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
+
+	    if (Input.GetKeyDown("e") && isAnObjectSelected == false)
+        {
+            currentlySelectedObject = (GameObject)Instantiate(selectableObjects[selectedObjectInArray], spawnPos, Quaternion.identity);
+            isAnObjectSelected = true;
+        }
+
+        if (Input.GetMouseButtonDown(1) && isAnObjectSelected == true)
+        {
+            Destroy(currentlySelectedObject);
+            isAnObjectSelected = false;
+            selectedObjectInArray = 0;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && isAnObjectSelected == true)
+        {
+            selectedObjectInArray++;
+
+            if (selectedObjectInArray > selectableObjects.Length - 1)
+            {
+                selectedObjectInArray = 0;
+            }
+
+            Destroy(currentlySelectedObject);
+            currentlySelectedObject = (GameObject)Instantiate(selectableObjects[selectedObjectInArray], spawnPos, Quaternion.identity);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0 && isAnObjectSelected == true)
+        {
+            selectedObjectInArray--;
+
+            if (selectedObjectInArray < 0)
+            {
+                selectedObjectInArray = selectableObjects.Length - 1;
+            }
+
+            Destroy(currentlySelectedObject);
+            currentlySelectedObject = (GameObject)Instantiate(selectableObjects[selectedObjectInArray], spawnPos, Quaternion.identity);
+        }
+    }
+}
---
+++ TemplateScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TemplateScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject finalObject;
+
+    private Vector2 mousePos;
+
+    [SerializeField]
+    private LayerMask allTilesLayer;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D rayHit = Physics2D.Raycast(transform.position, Vector2.zero, Mathf.Infinity, allTilesLayer);
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.collider.gameObject.tag == "GrassTile" && this.gameObject.tag == "HouseTemplate")
+                {
+                    Instantiate(finalObject, transform.position, Quaternion.identity);
+                }
+            }
+            else if (rayHit.collider == null && this.gameObject.tag == "GrassTemplate")
+            {
+                Instantiate(finalObject, transform.position, Quaternion.identity);
+            }
+        }
+	}
+}
