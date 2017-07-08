9fdc62a0b43c767e30e8199aaafbfd6abd2e8121
c00pala <c00p.b.coding@gmail.com>
2017-07-08 17:17:47 +1000

    Cleaned up TileChanger
    Accidentally left start and update in, not needed.

--- Tile Changer/TileChanger.cs
+++ Tile Changer/TileChanger.cs
@@ -5,14 +5,4 @@ using UnityEngine;
 public class TileChanger : MonoBehaviour {
 
     public bool changeMe;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
