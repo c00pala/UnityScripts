f1b666c1db4b1bbe1b23acaee82fb37859d55c19
c00pala <c00p.b.coding@gmail.com>
2017-07-12 11:27:25 +1000

    Added new subfolder.
    Added Deactivate Far From Player folder, scripts and readme.

---
+++ Deactivate Far From Player Tutorial/ItemActivator.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemActivator : MonoBehaviour {
+
+    // --------------------------------------------------
+    // Variables:
+
+    [SerializeField]
+    private int distanceFromPlayer;
+
+    private GameObject player;
+
+    public List<ActivatorItem> activatorItems;
+
+	// --------------------------------------------------
+
+	void Start()
+	{
+        player = GameObject.Find("Player");
+        activatorItems = new List<ActivatorItem>();
+
+        StartCoroutine("CheckActivation");
+	}
+
+	IEnumerator CheckActivation()
+    {
+        List<ActivatorItem> removeList = new List<ActivatorItem>();
+
+        if (activatorItems.Count > 0)
+        {
+            foreach (ActivatorItem item in activatorItems)
+            {
+                if (Vector3.Distance(player.transform.position, item.itemPos) > distanceFromPlayer)
+                {
+                    if (item.item == null)
+                    {
+                        removeList.Add(item);
+                    }
+                    else
+                    {
+                        item.item.SetActive(false);
+                    }
+                }
+                else
+                {
+                    if (item.item == null)
+                    {
+                        removeList.Add(item);
+                    }
+                    else
+                    {
+                        item.item.SetActive(true);
+                    }
+                }
+            }
+        }
+
+        yield return new WaitForSeconds(0.01f);
+
+        if (removeList.Count > 0)
+        {
+            foreach (ActivatorItem item in removeList)
+            {
+                activatorItems.Remove(item);
+            }
+        }
+
+        yield return new WaitForSeconds(0.01f);
+        StartCoroutine("CheckActivation");
+    }
+}
+
+public class ActivatorItem
+{
+    public GameObject item;
+    public Vector3 itemPos;
+}
+
---
+++ Deactivate Far From Player Tutorial/ReadMe.txt
@@ -0,0 +1,5 @@
+These are the scripts from my tutorial:
+Deactivate objects when from player.
+
+They can be implemented by following the steps in the tutorial:
+https://www.youtube.com/watch?v=kvI2NmlkRUo&lc=z12efhiq2uvzwlpeq22hunl4ztbuw5ebd04
