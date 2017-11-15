5f512cd24b5cc661094d2d544d2c3253b293b2e9
c00pala <c00p.b.coding@gmail.com>
2017-11-16 10:13:09 +1100

    Added new script
    Added PlayerMove.cs, a simple first person controller script.

---
+++ SimpleFPSController/PlayerMove.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    // To setup object for this script:
+    /* Create player object.
+     * Child game camera to player.
+     * Assign player to own layer. 
+     * Remove player's layer from the camera's culling mask.
+     * Add CharacterController component to player.
+     * Add this script to player and set variables.
+     */
+
+    // Cam look variables.
+    [SerializeField]
+    private float rotSpeedX; // Mouse X sensitivity control, set in editor.
+    [SerializeField]
+    private float rotSpeedY; // Mouse Y sensitivity control, set in editor.
+
+    [SerializeField]
+    private float rotDamp; // Damping value for camera rotation.
+
+    private float mY = 0f; // Mouse X.
+    private float mX = 0f; // Mouse Y.
+
+    // Player move variables.
+
+    [SerializeField]
+    private float walkSpeed; // Walk (normal movement) speed, set in editor.
+    [SerializeField]
+    private float runSpeed; // Run speed, set in editor.
+
+    private float currentSpeed; // Stores current movement speed.
+
+    [SerializeField]
+    private KeyCode runKey; // Run key, set in editor.
+
+    private CharacterController cc; // Reference to attached CharacterController.
+
+    [SerializeField]
+    private GameObject playerCamera; // Player cam, set in editor.
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        currentSpeed = walkSpeed;
+    }
+
+    private void LateUpdate()
+    {
+        // MOVEMENT - Hor and Ver axis:
+
+        // Get mouse axis.
+        mX += Input.GetAxis("Mouse X") * rotSpeedX * (Time.deltaTime * rotDamp);
+        mY += -Input.GetAxis("Mouse Y") * rotSpeedY * (Time.deltaTime * rotDamp);
+
+        // Clamp Y so player can't 'flip'.
+        mY = Mathf.Clamp(mY, -80, 80);
+
+        // Adjust rotation of camera and player's body.
+        // Rotate the camera on its X axis for up / down camera movement.
+        playerCamera.transform.localEulerAngles = new Vector3(mY, 0f, 0f);
+        // Rotate the player's body on its Y axis for left / right camera movement.
+        transform.eulerAngles = new Vector3(0f, mX, 0f);
+
+        // Get Hor and Ver input.
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        // Set speed to walk speed.
+        currentSpeed = walkSpeed;
+        // If player is pressing run key and moving forward, set speed to run speed.
+        if (Input.GetKey(runKey) && Input.GetKey(KeyCode.W)) currentSpeed = runSpeed;
+
+        // Get new move position based off input.
+        Vector3 moveDir = (transform.right * hor) + (transform.forward * ver);
+
+        // Move CharController. 
+        // .Move will not apply gravity, use SimpleMove if you want gravity.
+        cc.Move(moveDir * currentSpeed * Time.deltaTime);
+    }
+}
