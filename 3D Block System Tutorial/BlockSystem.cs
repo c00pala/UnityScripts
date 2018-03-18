0063e7106162f7522ed751d785a1cc37893ebb07
c00pala <c00p.b.coding@gmail.com>
2018-03-18 13:16:07 +1100

    Added 3d block tutorial scripts.

---
+++ 3D Block System Tutorial/BlockSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSystem : MonoBehaviour {
+
+    [SerializeField]
+    private BlockType[] allBlockTypes;
+
+    [HideInInspector]
+    public Dictionary<int, Block> allBlocks = new Dictionary<int, Block>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < allBlockTypes.Length; i++)
+        {
+            BlockType newBlockType = allBlockTypes[i];
+            Block newBlock = new Block(i, newBlockType.blockName, newBlockType.blockMat);
+            allBlocks[i] = newBlock;
+            Debug.Log("Block added to dictionary " + allBlocks[i].blockName);
+        }
+    }
+}
+
+public class Block
+{
+    public int blockID;
+    public string blockName;
+    public Material blockMaterial;
+
+    public Block (int id, string name, Material mat)
+    {
+        blockID = id;
+        blockName = name;
+        blockMaterial = mat;
+    }
+}
+
+[Serializable]
+public struct BlockType
+{
+    public string blockName;
+    public Material blockMat;
+}
---
+++ 3D Block System Tutorial/BuildingSystem.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSystem : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera playerCamera;
+
+    private bool buildModeOn = false;
+    private bool canBuild = false;
+
+    private BlockSystem bSys;
+
+    [SerializeField]
+    private LayerMask buildableSurfacesLayer;
+
+    private Vector3 buildPos;
+
+    private GameObject currentTemplateBlock;
+
+    [SerializeField]
+    private GameObject blockTemplatePrefab;
+    [SerializeField]
+    private GameObject blockPrefab;
+
+    [SerializeField]
+    private Material templateMaterial;
+
+    private int blockSelectCounter = 0;
+
+    private void Start()
+    {
+        bSys = GetComponent<BlockSystem>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            buildModeOn = !buildModeOn;
+
+            if (buildModeOn)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            blockSelectCounter++;
+            if (blockSelectCounter >= bSys.allBlocks.Count) blockSelectCounter = 0;
+        }
+
+        if (buildModeOn)
+        {
+            RaycastHit buildPosHit;
+
+            if (Physics.Raycast(playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)), out buildPosHit, 10, buildableSurfacesLayer))
+            {
+                Vector3 point = buildPosHit.point;
+                buildPos = new Vector3(Mathf.Round(point.x), Mathf.Round(point.y), Mathf.Round(point.z));
+                canBuild = true;
+            }
+            else
+            {
+                Destroy(currentTemplateBlock.gameObject);
+                canBuild = false;
+            }
+        }
+
+        if (!buildModeOn && currentTemplateBlock != null)
+        {
+            Destroy(currentTemplateBlock.gameObject);
+            canBuild = false;
+        }
+
+        if (canBuild && currentTemplateBlock == null)
+        {
+            currentTemplateBlock = Instantiate(blockTemplatePrefab, buildPos, Quaternion.identity);
+            currentTemplateBlock.GetComponent<MeshRenderer>().material = templateMaterial;
+        }
+
+        if (canBuild && currentTemplateBlock != null)
+        {
+            currentTemplateBlock.transform.position = buildPos;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlaceBlock();
+            }
+        }
+    }
+
+    private void PlaceBlock()
+    {
+        GameObject newBlock = Instantiate(blockPrefab, buildPos, Quaternion.identity);
+        Block tempBlock = bSys.allBlocks[blockSelectCounter];
+        newBlock.name = tempBlock.blockName + "-Block";
+        newBlock.GetComponent<MeshRenderer>().material = tempBlock.blockMaterial;
+    }
+}
